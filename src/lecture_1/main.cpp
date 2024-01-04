500f3f45576edefeac658b077227ad03bdfb39d9
Jens Weber <jweber1094@gmail.com>
2024-01-04 12:18:52 +0000

    check if boost works

--- src/lecture_1/main.cpp
+++ src/lecture_1/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
+#include <boost/lexical_cast.hpp>
 
 int main() {
     std::cout << "Hello World!\n";
+    int foo = 23;
+    std::string bar = boost::lexical_cast<std::string>(foo);
+    std::cout << "bar is " << bar << std::endl;
+
     return 0;
 }
